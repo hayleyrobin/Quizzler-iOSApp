7c9577da51e750702a57ab9c04f7b0cc602f70cc
Hayley Robinson <hayley.robinson63@myhunter.cuny.edu>
2022-01-08 18:20:52 -0500

    Timer to show background color of button before resetting to new question

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -43,10 +43,10 @@ class ViewController: UIViewController {
         let actualAnswer = quiz[questionNumber].answer
         
         if userAnswer == actualAnswer {
-            print("Right")
+            sender.backgroundColor = UIColor.green
         }
         else{
-            print("Wrong")
+            sender.backgroundColor = UIColor.red
         }
         
         if(questionNumber < quiz.count-1) {
@@ -55,12 +55,16 @@ class ViewController: UIViewController {
         }else{
             questionNumber = 0
         }
-        updateUI()
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
     }
     
-    func updateUI() {
+    @objc func updateUI() {
         questionLabel.text = quiz[questionNumber].text
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
     }
+    
 
 }
 
